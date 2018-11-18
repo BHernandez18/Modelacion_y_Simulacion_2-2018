3cd488c023eac56077b04a1610e2f2a0653449ee
Benjamín Hernández <benjamin.hernandez@usach.cl>
2018-11-18 03:02:57 -0300

    Se ha cambiado la 3ra función
    >> Se modifica la 3ra función por otra.

--- Laboratorio 2/parte_1.m
+++ Laboratorio 2/parte_1.m
@@ -22,8 +22,8 @@
 % 2) (d^2 y/dt^2) + 4*(dy/dt) + 3*y - (d^2 u/dt^2) - 8*(du/dt) - u = 0
 %       y(0) = 1, y'(0) = 1, u(0) = 0, u'(0) = 0
 %
-% 3) (d^3 y/dt^3) + 2*(d^2 y/dt^2) + 4*(dy/dt) + 8y = u
-%       y(0) = 3, y'(0) = 9, y''(0) = 1
+% 3) (d^3 y/dt^3) + 2*(d^2 y/dt^2) + y = 5*(du/dt)
+%       y(0) = 1, y'(0) = 1, y''(0) = 0, u(0) = 0
 %
 % El procedimiento consiste en obtener la función de transferencia asociada
 % a cada una de las funciones, para lo cual se emplea la Transformada de
@@ -32,7 +32,7 @@
 %
 % 1) H1(s) = s / (2*s + 1)
 % 2) H2(s) = (s^2 + 8*s + 1) / (s^2 + 4*s + 3)
-% 3) H3(s) = 1 / (s^3 + 2*s^2 + 4*s + 1)
+% 3) H3(s) = 5*s / (s^3 + 2*s^2 + 1)
 %
 % A continuación, se definen los vectores que representarán el numerador y
 % denominador de cada función de transferencia conseguida.
@@ -52,8 +52,14 @@ H2_abierto = tf(numerador_FdeT_2, denominador_FdeT_2);
 H2_cerrado = feedback(H2_abierto, 1);
 
 % >> H3(s) -> Función de tercer orden. <<
-numerador_FdeT_3 = [0 0 0 1];
-denominador_FdeT_3 = [1 2 4 8];
+numerador_FdeT_3 = [0 0 5 0];
+denominador_FdeT_3 = [1 2 0 1];
 
 H3_abierto = tf(numerador_FdeT_3, denominador_FdeT_3);
 H3_cerrado = feedback(H3_abierto, 1);
+
+% ===========================
+% == Obtención de gráficos ==
+%
+figure(1);
+step(H1_abierto);
