e3f97ecf45a296230d4346e47949fe04bf2793d6
Benjamín Hernández <benjamin.hernandez@usach.cl>
2018-11-18 15:23:26 -0300

    Primera parte finalizada - Inicio de la segunda
    >> Se termina el script para la primera parte del laboratorio y se deja el script para desarrollar la segunda.

--- Laboratorio 2/parte_1.m
+++ Laboratorio 2/parte_1.m
@@ -42,7 +42,7 @@ numerador_FdeT_1 = [1 0];
 denominador_FdeT_1 = [2 1];
 
 H1_abierto = tf(numerador_FdeT_1, denominador_FdeT_1);
-H2_cerrado = feedback(H1_abierto, 1);
+H1_cerrado = feedback(H1_abierto, 1);
 
 % >> H2(s) -> Función de segundo orden. <<
 numerador_FdeT_2 = [1 8 1];
@@ -60,6 +60,113 @@ H3_cerrado = feedback(H3_abierto, 1);
 
 % ===========================
 % == Obtención de gráficos ==
+
+%
+% >> Función de Transferencia H1(s) <<
 %
 figure(1);
 step(H1_abierto);
+title({'\textbf{Respuesta al impulso para} $H_{1}(s) = \frac{s}{2s + 1}$ [Abierto]'},...
+    'Interpreter','latex',...
+    'FontSize', 15);
+xlabel('Tiempo');
+ylabel('Amplitud');
+saveas(gcf, 'grafico_H1s_abierto.png');
+%-------------------------------------
+figure(2);
+step(H1_cerrado);
+title({'\textbf{Respuesta al impulso para} $H_{1}(s) = \frac{s}{2s + 1}$ [Cerrado]'},...
+    'Interpreter','latex',...
+    'FontSize', 15);
+xlabel('Tiempo');
+ylabel('Amplitud');
+saveas(gcf, 'grafico_H1s_cerrado.png');
+
+%
+% >> Función de Transferencia H2(s) <<
+%
+figure(3);
+step(H2_abierto);
+title({'\textbf{Respuesta al impulso para} $H_{2}(s) = \frac{s^2 + 8s + 1}{s^2 + 4s + 3}$ [Abierto]'},...
+    'Interpreter','latex',...
+    'FontSize', 15);
+xlabel('Tiempo');
+ylabel('Amplitud');
+saveas(gcf, 'grafico_H2s_abierto.png');
+%-------------------------------------
+figure(4);
+step(H2_cerrado);
+title({'\textbf{Respuesta al impulso para} $H_{2}(s) = \frac{s^2 + 8s + 1}{s^2 + 4s + 3}$ [Cerrado]'},...
+    'Interpreter','latex',...
+    'FontSize', 15);
+xlabel('Tiempo');
+ylabel('Amplitud');
+saveas(gcf, 'grafico_H2s_cerrado.png');
+
+%
+% >> Función de Transferencia H3(s) <<
+%
+figure(5);
+step(H3_abierto);
+title({'\textbf{Respuesta al impulso para} $H_{3}(s) = \frac{5s}{s^3 + 2s^2 + 1}$ [Abierto]'},...
+    'Interpreter','latex',...
+    'FontSize', 15);
+xlabel('Tiempo');
+ylabel('Amplitud');
+saveas(gcf, 'grafico_H3s_abierto.png');
+%-------------------------------------
+figure(6);
+step(H3_cerrado);
+title({'\textbf{Respuesta al impulso para} $H_{3}(s) = \frac{5s}{s^3 + 2s^2 + 1}$ [Cerrado]'},...
+    'Interpreter','latex',...
+    'FontSize', 15);
+xlabel('Tiempo');
+ylabel('Amplitud');
+saveas(gcf, 'grafico_H3s_cerrado.png');
+
+% ==========================================
+% == Obtención de ganancia, ceros y polos ==
+
+[num, den] = tfdata(H1_abierto, 'v');
+[Z1_abierto, P1_abierto, K1_abierto] = tf2zp(num,den);
+[num, den] = tfdata(H1_cerrado, 'v');
+[Z1_cerrado, P1_cerrado, K1_cerrado] = tf2zp(num,den);
+
+% Ganancia, ceros y polos - funcion 2do orden
+[num, den] = tfdata(H2_abierto, 'v');
+[Z2_abierto, P2_abierto, K2_abierto] = tf2zp(num,den);
+[num, den] = tfdata(H2_cerrado, 'v');
+[Z2_cerrado, P2_cerrado, K2_cerrado] = tf2zp(num,den);
+
+% Ganancia, ceros y polos - funcion 3er orden
+[num, den] = tfdata(H3_abierto, 'v');
+[Z3_abierto, P3_abierto, K3_abierto] = tf2zp(num,den);
+[num, den] = tfdata(H3_cerrado, 'v');
+[Z3_cerrado, P3_cerrado, K3_cerrado] = tf2zp(num,den);
+
+fprintf(">> Información respecto a las Funciones de Transferencia <<\n");
+fprintf("-----------------------------------------------------------\n");
+fprintf("Cero (H1_abierto): %.4f\n", Z1_abierto);
+fprintf("Polo (H1_abierto): %.4f\n", P1_abierto);
+fprintf("Ganancia (H1_abierto): %.4f\n\n", K1_abierto);
+
+fprintf("Cero (H1_cerrado): %.4f\n", Z1_cerrado);
+fprintf("Polo (H1_cerrado): %.4f\n", P1_cerrado);
+fprintf("Ganancia (H1_cerrado): %.4f\n\n", K1_cerrado);
+fprintf("-----------------------------------------------------------\n");
+fprintf("Cero (H2_abierto): %.4f\n", Z2_abierto);
+fprintf("Polo (H2_abierto): %.4f\n", P2_abierto);
+fprintf("Ganancia (H2_abierto): %.4f\n\n", K2_abierto);
+
+fprintf("Cero (H2_cerrado): %.4f\n", Z2_cerrado);
+fprintf("Polo (H2_cerrado): %.4f\n", P2_cerrado);
+fprintf("Ganancia (H2_cerrado): %.4f\n\n", K2_cerrado);
+fprintf("-----------------------------------------------------------\n");
+fprintf("Cero (H3_abierto): %.4f\n", Z3_abierto);
+fprintf("Polo (H3_abierto): %.4f%+.4fi\n", real(P3_abierto), imag(P3_abierto));
+fprintf("Ganancia (H3_abierto): %.4f\n\n", K3_abierto);
+
+fprintf("Cero (H3_cerrado): %.4f\n", Z3_cerrado);
+fprintf("Polo (H3_cerrado): %.4f%+.4fi\n", real(P3_cerrado), imag(P3_cerrado));
+fprintf("Ganancia (H3_cerrado): %.4f\n\n", K3_cerrado);
+fprintf("-----------------------------------------------------------\n");
---
+++ Laboratorio 2/parte_2.m
@@ -0,0 +1,17 @@
+%
+% Laboratorio 2 - Modelación y Simulación
+% << Análisis de sistemas >>
+%
+% Integrantes:  Pablo Cáceres Luzanto
+%               Benjamín Hernández Cortés
+%
+% Fecha: 22 de noviembre de 2018
+% Profesor: Gonzalo Acuña
+% Ayudante: Francisco Muñoz
+%
+
+% ===================
+% == Segunda parte ==
+%
+% Se solicita graficar la respuesta al escalón para el diagrama de bloque
+% mostrado en el enunciado.
