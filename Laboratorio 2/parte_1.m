4316e8a8bbb1ea1d026061331ca5b682c60d9d5b
Benjamín Hernández <benjamin.hernandez@usach.cl>
2018-11-20 20:08:00 -0300

    Termino de la segunda parte
    >> Se finaliza la segunda parte del laboratorio
    >> Se añade el 'Tiempo de Estabilización' para cada una de las funciones de transferencia de la parte 1.

--- Laboratorio 2/parte_1.m
+++ Laboratorio 2/parte_1.m
@@ -71,6 +71,7 @@ title({'\textbf{Respuesta al impulso para} $H_{1}(s) = \frac{s}{2s + 1}$ [Abiert
     'FontSize', 15);
 xlabel('Tiempo');
 ylabel('Amplitud');
+grid on;
 saveas(gcf, 'grafico_H1s_abierto.png');
 %-------------------------------------
 figure(2);
@@ -80,6 +81,7 @@ title({'\textbf{Respuesta al impulso para} $H_{1}(s) = \frac{s}{2s + 1}$ [Cerrad
     'FontSize', 15);
 xlabel('Tiempo');
 ylabel('Amplitud');
+grid on;
 saveas(gcf, 'grafico_H1s_cerrado.png');
 
 %
@@ -92,6 +94,7 @@ title({'\textbf{Respuesta al impulso para} $H_{2}(s) = \frac{s^2 + 8s + 1}{s^2 +
     'FontSize', 15);
 xlabel('Tiempo');
 ylabel('Amplitud');
+grid on;
 saveas(gcf, 'grafico_H2s_abierto.png');
 %-------------------------------------
 figure(4);
@@ -101,6 +104,7 @@ title({'\textbf{Respuesta al impulso para} $H_{2}(s) = \frac{s^2 + 8s + 1}{s^2 +
     'FontSize', 15);
 xlabel('Tiempo');
 ylabel('Amplitud');
+grid on;
 saveas(gcf, 'grafico_H2s_cerrado.png');
 
 %
@@ -113,6 +117,7 @@ title({'\textbf{Respuesta al impulso para} $H_{3}(s) = \frac{5s}{s^3 + 2s^2 + 1}
     'FontSize', 15);
 xlabel('Tiempo');
 ylabel('Amplitud');
+grid on;
 saveas(gcf, 'grafico_H3s_abierto.png');
 %-------------------------------------
 figure(6);
@@ -122,51 +127,64 @@ title({'\textbf{Respuesta al impulso para} $H_{3}(s) = \frac{5s}{s^3 + 2s^2 + 1}
     'FontSize', 15);
 xlabel('Tiempo');
 ylabel('Amplitud');
+grid on;
 saveas(gcf, 'grafico_H3s_cerrado.png');
 
-% ==========================================
-% == Obtención de ganancia, ceros y polos ==
+% ====================================================================
+% == Obtención de ganancia, ceros, polos y tiempo de estabilización ==
 
 [num, den] = tfdata(H1_abierto, 'v');
 [Z1_abierto, P1_abierto, K1_abierto] = tf2zp(num,den);
 [num, den] = tfdata(H1_cerrado, 'v');
 [Z1_cerrado, P1_cerrado, K1_cerrado] = tf2zp(num,den);
+stepinfo_H1_abierto = stepinfo(H1_abierto);
+stepinfo_H1_cerrado = stepinfo(H1_cerrado);
 
 % Ganancia, ceros y polos - funcion 2do orden
 [num, den] = tfdata(H2_abierto, 'v');
 [Z2_abierto, P2_abierto, K2_abierto] = tf2zp(num,den);
 [num, den] = tfdata(H2_cerrado, 'v');
 [Z2_cerrado, P2_cerrado, K2_cerrado] = tf2zp(num,den);
+stepinfo_H2_abierto = stepinfo(H2_abierto);
+stepinfo_H2_cerrado = stepinfo(H2_cerrado);
 
 % Ganancia, ceros y polos - funcion 3er orden
 [num, den] = tfdata(H3_abierto, 'v');
 [Z3_abierto, P3_abierto, K3_abierto] = tf2zp(num,den);
 [num, den] = tfdata(H3_cerrado, 'v');
 [Z3_cerrado, P3_cerrado, K3_cerrado] = tf2zp(num,den);
+stepinfo_H3_abierto = stepinfo(H3_abierto);
+stepinfo_H3_cerrado = stepinfo(H3_cerrado);
 
 fprintf(">> Información respecto a las Funciones de Transferencia <<\n");
 fprintf("-----------------------------------------------------------\n");
 fprintf("Cero (H1_abierto): %.4f\n", Z1_abierto);
 fprintf("Polo (H1_abierto): %.4f\n", P1_abierto);
-fprintf("Ganancia (H1_abierto): %.4f\n\n", K1_abierto);
+fprintf("Ganancia (H1_abierto): %.4f\n", K1_abierto);
+fprintf("Tiempo de estabilización (H1_abierto): %.4f\n\n", stepinfo_H1_abierto.SettlingTime);
 
 fprintf("Cero (H1_cerrado): %.4f\n", Z1_cerrado);
 fprintf("Polo (H1_cerrado): %.4f\n", P1_cerrado);
-fprintf("Ganancia (H1_cerrado): %.4f\n\n", K1_cerrado);
+fprintf("Ganancia (H1_cerrado): %.4f\n", K1_cerrado);
+fprintf("Tiempo de estabilización (H1_cerrado): %.4f\n\n", stepinfo_H1_cerrado.SettlingTime);
 fprintf("-----------------------------------------------------------\n");
 fprintf("Cero (H2_abierto): %.4f\n", Z2_abierto);
 fprintf("Polo (H2_abierto): %.4f\n", P2_abierto);
-fprintf("Ganancia (H2_abierto): %.4f\n\n", K2_abierto);
+fprintf("Ganancia (H2_abierto): %.4f\n", K2_abierto);
+fprintf("Tiempo de estabilización (H2_abierto): %.4f\n\n", stepinfo_H2_abierto.SettlingTime);
 
 fprintf("Cero (H2_cerrado): %.4f\n", Z2_cerrado);
 fprintf("Polo (H2_cerrado): %.4f\n", P2_cerrado);
-fprintf("Ganancia (H2_cerrado): %.4f\n\n", K2_cerrado);
+fprintf("Ganancia (H2_cerrado): %.4f\n", K2_cerrado);
+fprintf("Tiempo de estabilización (H2_cerrado): %.4f\n\n", stepinfo_H2_cerrado.SettlingTime);
 fprintf("-----------------------------------------------------------\n");
 fprintf("Cero (H3_abierto): %.4f\n", Z3_abierto);
 fprintf("Polo (H3_abierto): %.4f%+.4fi\n", real(P3_abierto), imag(P3_abierto));
-fprintf("Ganancia (H3_abierto): %.4f\n\n", K3_abierto);
+fprintf("Ganancia (H3_abierto): %.4f\n", K3_abierto);
+fprintf("Tiempo de estabilización (H3_abierto): %.4f\n\n", stepinfo_H3_abierto.SettlingTime);
 
 fprintf("Cero (H3_cerrado): %.4f\n", Z3_cerrado);
 fprintf("Polo (H3_cerrado): %.4f%+.4fi\n", real(P3_cerrado), imag(P3_cerrado));
-fprintf("Ganancia (H3_cerrado): %.4f\n\n", K3_cerrado);
+fprintf("Ganancia (H3_cerrado): %.4f\n", K3_cerrado);
+fprintf("Tiempo de estabilización (H1_cerrado): %.4f\n\n", stepinfo_H3_cerrado.SettlingTime);
 fprintf("-----------------------------------------------------------\n");
--- Laboratorio 2/parte_2.m
+++ Laboratorio 2/parte_2.m
@@ -15,3 +15,75 @@
 %
 % Se solicita graficar la respuesta al escalón para el diagrama de bloque
 % mostrado en el enunciado.
+%
+% Funciones de Transferencia asociadas.
+% -------------------------------------
+H1 = tf([0 4 0], [5 0 2]);      %     4s / 5s^2 + 2
+H2 = tf([0 4], [1 6]);          %      4 / s + 6
+H3 = tf([0 0 1 1], [3 5 0 0]);  %  s + 1 / 3s^3 + 5s^2
+H4 = tf([0 1], [7 4]);          %      1 / 7s + 4
+H5 = tf([0 0 5 4], [5 1 0 4]);  % 5s + 4 / 5s^3 + s^2 + 4
+H6 = tf([0 0 1 3], [1 6 0 5]);  % 2s + 3 / s^3 + 6s^2 + 5
+
+% Obtención del diagrama de bloque asociado.
+% ------------------------------------------
+% Sistemas en Paralelo - Función 4 y 5
+H45 = parallel(H4, H5);
+
+% Retroalimentación - Función 3
+H3F = feedback(H3, 1, +1);
+
+% Sistemas en Serie - H3F y H45 
+H345 = series(H3F, H45);
+
+% Sistemas en Serie - H345 y Función 6
+H3456 = series(H345, H6);
+
+% Sistemas en Paralelo - Función 1 y 2
+H12 = parallel(H1, H2);
+
+% Sistemas en Paralelo - H12 y H3456
+Hfinal = parallel(H12, H3456);
+
+% ===========================
+% == Obtención de gráficos ==
+
+%
+% >> Función de Transferencia Hfinal(s) <<
+%
+figure(1);
+step(Hfinal);
+title({'\textbf{Respuesta al impulso para} $H_{final}(s)$'},...
+    'Interpreter','latex',...
+    'FontSize', 13);
+xlabel('Tiempo');
+ylabel('Amplitud');
+grid on;
+saveas(gcf, 'grafico_Hfinal.png');
+
+% ==================================================
+% == Polinomios de las funciones de transferencia ==
+
+% Sistemas en Paralelo - Función 4 y 5
+[num, den] = tfdata(H45, 'v');
+fprintf("H45(s) = %s / %s\n\n", poly2sym(num), poly2sym(den));
+
+% Retroalimentación - Función 3
+[num, den] = tfdata(H3F, 'v');
+fprintf("H3F(s) = %s / %s\n\n", poly2sym(num), poly2sym(den));
+
+% Sistemas en Serie - H3F y H45 
+[num, den] = tfdata(H345, 'v');
+fprintf("H345(s) = %s / %s\n\n", poly2sym(num), poly2sym(den));
+
+% Sistemas en Serie - H345 y Función 6
+[num, den] = tfdata(H3456, 'v');
+fprintf("H3456(s) = %s / %s\n\n", poly2sym(num), poly2sym(den));
+
+% Sistemas en Paralelo - Función 1 y 2
+[num, den] = tfdata(H12, 'v');
+fprintf("H12(s) = %s / %s\n\n", poly2sym(num), poly2sym(den));
+
+% Sistemas en Paralelo - H12 y H3456
+[num, den] = tfdata(Hfinal, 'v');
+fprintf("Hfinal(s) = %s / %s\n", poly2sym(num), poly2sym(den));
